2e4676349591e37bb3eb38deb97b9acd948aca56
Rafa <junior80015@gmail.com>
2025-11-19 19:24:06 +0800

    [-] 去除無用code

--- 3.service/system/01-status.go
+++ 3.service/system/01-status.go
@@ -2,24 +2,9 @@ package systemService
 
 import (
 	"batchLog/0.core/global"
-	"fmt"
-	"time"
 )
 
 func SystemStatus() (map[string]interface{}, error) {
-	const timeout = 2 * time.Second
-	start := time.Now()
-	for {
-		if global.ActiveDevicesLock.TryLock() {
-			break
-		}
-		if time.Since(start) > timeout {
-			// 鎖超時
-			return nil, fmt.Errorf("警告：MqttOnlineDevice() 嘗試加鎖超過 2 秒，放棄。")
-		}
-		time.Sleep(10 * time.Millisecond)
-	}
-	defer global.ActiveDevicesLock.Unlock()
 	status := "連線正常"
 	if !global.IsConnected.Load() {
 		status = "連線斷開"
